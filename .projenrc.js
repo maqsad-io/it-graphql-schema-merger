23fcef65524b3cf217cebb2d9c5800a4abb112fd
hassan-maqsad <87966807+hassan-maqsad@users.noreply.github.com>
2022-05-31 02:59:21 +0500

    Updated projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,14 @@
-const { typescript } = require("projen");
+const { typescript } = require('projen');
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: "main",
-  name: "sample",
+  defaultReleaseBranch: 'main',
+  name: 'graphql-schema-merger',
+  packageName: 'graphql-schema-merger',
+  description: 'A simple helper to merge multiple graphql schemas with directives',
+  authorName: 'Talha Siddiqui & Hassan Abbasi',
+  authorOrganization: 'Maqsad',
+  authorEmail: 'hassan@maqsad.io',
+  authorUrl: 'maqsad.io',
+  outdir: 'src/lib',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
