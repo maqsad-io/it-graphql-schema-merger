8b76c10bf0aefb55bd75145bf98d02e8b7cb58d3
hassan-maqsad <87966807+hassan-maqsad@users.noreply.github.com>
2022-05-31 03:03:22 +0500

    Updated Readme

--- .projenrc.js
+++ .projenrc.js
@@ -10,6 +10,7 @@ const project = new typescript.TypeScriptProject({
   authorUrl: 'maqsad.io',
   outdir: 'src/lib',
 
+
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
